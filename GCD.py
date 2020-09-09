1e85d5597fee31d83b3c84b5d9d6bd22ab887b3b
Ed Klyne <edk53@scarletmail.rutgers.edu>
2020-09-09 16:23:22 -0400

    Started plotting, not finished

--- GCD.py
+++ GCD.py
@@ -1,17 +1,22 @@
+import matplotlib.pyplot as plt
+
 def gcd(x, y):
     i=1
     print(x+y)
+    plt.plot([i], [x+y], 'ro')
     
     while (y!=0):
         x,y = y, (x%y)
         
         if (i%2 == 0):
             print(x+y)
+            plt.plot([i], [x+y], 'ro')
       
         i+=1
       
     return x, (i-1)
 
 print(gcd(55046789, 34165))
+plt.show()
 
 
