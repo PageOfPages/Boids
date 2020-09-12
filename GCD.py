24f033bea0840d61ab9a12c4aa05597a241bf8ff
PageOfPages <51131231+PageOfPages@users.noreply.github.com>
2020-09-12 16:07:14 -0400

    Update GCD.py

--- GCD.py
+++ GCD.py
@@ -3,17 +3,18 @@ import matplotlib.pyplot as plt
 def gcd(x, y):
     i=1
     print(x+y)
-    plt.plot([i], [x+y], "bo")
-
+    points = {i:x+y}
+    
     while (y):
         x,y = y, x%y
         
         if (i%2 == 0):
             print(x+y)
-            plt.plot([i], [x+y], 'ro')
+            points[i] = x+y
       
         i+=1
-      
+    
+    plt.plot(list(points.keys()), list(points.values()))
     return x, (i-1)
 
 print(gcd(55046789, 34165))
